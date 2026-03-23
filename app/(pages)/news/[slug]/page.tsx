34762505440d3219eef6dbcaf27909608c065fd7
adrian-beston <adrian@big-pixel.com>
2026-03-23 11:28:09 +0000

    Formatting and updates to news

--- app/(pages)/news/[slug]/page.tsx
+++ app/(pages)/news/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { getNewsSlugs } from '@/app/lib/mdx';
 import type { Metadata } from 'next';
+import { format } from 'date-fns';
 
 export async function generateMetadata({
   params,
@@ -10,16 +11,26 @@ export async function generateMetadata({
   const { slug } = await params;
   const { frontmatter } = await import(`@/app/markdown/news/${slug}.mdx`);
   return {
-    title: frontmatter.metaTitle,
+    title: frontmatter.title,
     description: frontmatter.description,
-    keywords: frontmatter.keywords,
-    alternates: { canonical: frontmatter.canonicalUrl },
+    alternates: {
+      canonical: frontmatter.canonicalUrl,
+    },
     openGraph: {
       title: frontmatter.metaTitle,
       description: frontmatter.description,
-      images: [{ url: frontmatter.ogImage }],
+      url: frontmatter.canonicalUrl,
+      images: [
+        {
+          url: `/images/news/${frontmatter.ogImage}` || `/images/news/${frontmatter.cover}`,
+          alt: frontmatter.title || '',
+          width: 1200,
+          height: 630,
+        },
+      ],
       type: 'article',
     },
+    robots: { index: frontmatter.noIndex, follow: true },
   };
 }
 
@@ -48,8 +59,26 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
         className="rounded-lg object-cover"
         priority
       />
-      <div className="mt-8 flex flex-col prose-slate prose lg:prose-lg xl:prose-xl dark:prose-invert font-sans font-normal mx-auto">
-        <Post />
+      <div className="px-4 py-8 max-w-3xl mx-auto sm:px-6 sm:py-10 lg:max-w-4xl lg:py-12 xl:max-w-6xl">
+        <h1 className="font-headline font-black text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
+          {frontmatter.title}
+        </h1>
+        <p className="mt-4 text-lg text-metal-500">by {frontmatter.author}</p>
+        <p className="mt-4 text-sm text-metal-500">
+          Published {format(new Date(frontmatter.datePublished), 'do MMMM yyyy')}
+          {frontmatter.dateModified && (
+            <span className="italic">
+              {' '}
+              &mdash; updated {format(new Date(frontmatter.dateModified), 'do MMMM yyyy')}
+            </span>
+          )}
+        </p>
+        <p className="mt-2 text-sm text-metal-500">
+          Reading time: {Math.ceil(frontmatter.readingTime)} minutes
+        </p>
+        <article className="mt-8 prose prose-sm sm:prose-base lg:prose-lg xl:prose-xl prose-slate dark:prose-invert font-light mx-auto">
+          <Post />
+        </article>
       </div>
 
       <script
